ccdcf7cce6009ef73ca71dd308077c36f7426419
Leonn1997 <leon.gottschick@web.de>
2018-08-20 11:30:47 +0200

    added gitignore

---
+++ lumen-service/app/Http/Controllers/HealthController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leong
+ * Date: 19.08.2018
+ * Time: 13:20
+ */
+
+namespace App\Http\Controllers;
+
+
+class HealthController
+{
+
+}
---
+++ lumen-service/app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leong
+ * Date: 20.08.2018
+ * Time: 11:05
+ */
+
+namespace App\Http\Middleware;
+
+
+class CorsMiddleware
+{
+
+}
